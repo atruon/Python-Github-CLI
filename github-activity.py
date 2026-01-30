f6bd0077a12573ecdb61deffbce38ac020a7eed7
Anson Truong <atruon07@uoguelph.ca>
2026-01-30 18:41:54 -0500

    Added Error Messages and started formatting proper output

--- github-activity.py
+++ github-activity.py
@@ -3,11 +3,14 @@ import requests
 import sys
 
 def outputActivity(username):
-    if(len(username) > 0):
-        requestURL = "https://api.github.com/users/"+username+"/events"
-        response = requests.get(requestURL)
+    requestURL = "https://api.github.com/users/"+username+"/events"
+    response = requests.get(requestURL)
+    if (response.status_code == 200):
+        print("Output: ")
         print(response.text)
-        
+    elif (response.status_code == 404):
+        print("Error: Invalid Username")
+
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
         outputActivity(sys.argv[1])
