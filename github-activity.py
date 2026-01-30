1d1ebdf6c40c5b26638472e026fe15e2ca72eca9
Anson Truong <atruon07@uoguelph.ca>
2026-01-30 18:49:37 -0500

    Added Parse for WatchEvents

--- github-activity.py
+++ github-activity.py
@@ -5,11 +5,20 @@ import sys
 def outputActivity(username):
     requestURL = "https://api.github.com/users/"+username+"/events"
     response = requests.get(requestURL)
-    if (response.status_code == 200):
-        print("Output: ")
-        print(response.text)
-    elif (response.status_code == 404):
+    if (response.status_code == 404):
         print("Error: Invalid Username")
+        return
+    parsed_events = []
+    events = response.json()
+    for event in events:
+        parsed = {
+            "type": event["type"],
+            "actor": event["actor"],
+            "repo": event["repo"]
+        }
+        if parsed["type"] == "WatchEvent":
+            print("Test")
+    
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
