00bd5e107589bbea22f4358b5b772a7204a55ae9
Anson Truong <atruon07@uoguelph.ca>
2026-01-30 19:34:26 -0500

    Added parsing for WatchEvent, PushEvent, CreateEvent, and ForkEvent

--- github-activity.py
+++ github-activity.py
@@ -1,4 +1,5 @@
 import json
+from weakref import ref
 import requests
 import sys
 
@@ -14,14 +15,29 @@ def outputActivity(username):
         parsed = {
             "type": event["type"],
             "actor": event["actor"],
-            "repo": event["repo"]
+            "repo": event["repo"],
+            "payload": event["payload"],
+            "name": event["repo"]["name"]
         }
         if parsed["type"] == "WatchEvent":
-            print("Test")
-    
+            parsed_events.append("Started on " + parsed["name"])
+        elif parsed["type"] == "PushEvent":
+            parsed_events.append("Pushed commits to " + parsed["name"])
+        elif parsed["type"] == "CreateEvent":
+            parsed_events.append("Created repo - " + parsed["name"] + ". Description: " +
+                                 parsed["payload"]["description"] )
+        elif parsed["type"] == "ForkEvent":
+            parsed_events.append(parsed["actor"]["display_login"] + " " + parsed["payload"]["action"] +
+                                 " " + parsed["payload"]["forkee"]["full_name"])
+    return parsed_events
+
+def printParsedEvents(arr):
+    for string in arr:
+        print(string)
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
-        outputActivity(sys.argv[1])
+        allEvents = outputActivity(sys.argv[1])
+        printParsedEvents(allEvents)
     else:
         print("Error: No Username provided")
